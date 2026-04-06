23519c4b5aba2039ecf944b009e7d8c28c3c1b59
KM99999 <keisukeHashimotoDev@outlook.com>
2026-04-05 21:12:11 -0400

    second

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -47,23 +47,20 @@ const About = () => {
           variants={fadeIn("", "", 0.1, 1)}
           className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
         >
-          Hello! I'm Jeff Jiang, a New York-based Full-Stack Developer
+          Hello! I'm keisukeHashimoto, a Japan-based Full-Stack Developer
           proficient in an array of modern technologies including Python 3,
           JavaScript ES6+, SQL, Django 4, MongoDB, Node.js, React, and more. I'm
           passionate about creating dynamic, efficient, and user-friendly web
           applications. I have hands-on experience in developing e-commerce
           platforms, utilizing technologies such as Next.js, Strapi, Tailwind,
           Firebase, and Redux. I am also experienced in managing the full
-          project lifecycle from setup to deployment. My journey in tech is
-          supported by my prior experiences, including my time as a Police
-          Officer at NYPD and a Restaurant Manager, which honed my skills in
-          teamwork, problem-solving, and operations management. Feel free to
+          project lifecycle from setup to deployment.  Feel free to
           explore my projects and get in touch at{" "}
           <a
-            href="mailto:jeff.jiang13@gmail.com"
+            href="mailto:keisukeHashimotoDev@outlook.com"
             className="animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent font-black"
           >
-            jeff.jiang13@gmail.com.
+            keisukeHashimotoDev@outlook.com.
           </a>{" "}
           I'm always open to new opportunities and collaborations!{" "}
         </p>
@@ -77,8 +74,8 @@ const About = () => {
               className="bg-tertiary rounded-[20px] min-h-[250px] flex justify-evenly items-center flex-col overflow-hidden"
             >
               <img
-                src={jeff}
-                alt="jeff"
+                src={keisuke}
+                alt="keisuke"
                 className="w-full h-full object-contain"
               />
             </div>
